c4d2ffbfbe2ea9d6903b89c768d3c03b9e461e33
Majura-fi <12672127+Majura-fi@users.noreply.github.com>
2024-09-20 14:35:39 +0300

    Delete post-build.bat

--- MapperSource/post-build.bat
+++
@@ -1,7 +0,0 @@
-Copy ^
-    "C:\Program Files (x86)\Steam\steamapps\common\Green Hell\Mods\MapperSource\MapperSource\bin\Debug\net48\Mapper.dll" ^
-    "C:\Program Files (x86)\Steam\steamapps\common\Green Hell\Mods\Mapper\Mapper.dll" 
-
-Copy ^
-    "C:\Program Files (x86)\Steam\steamapps\common\Green Hell\Mods\MapperSource\MapperSource\Info.json" ^
-    "C:\Program Files (x86)\Steam\steamapps\common\Green Hell\Mods\Mapper\Info.json"
